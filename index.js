a428f388720e0612a2cf94d9a10b5030792aeac9
Mel Mel Mom <melmelmomof4@gmail.com>
2024-02-18 13:52:03 -0500

    all options included

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const viewRole = async () => {
 
 const viewDepartment = async () => {
   const result = await sequelize.query("SELECT * FROM department");
+  console.log(result[0]);
+  return result[0];
 
 }
 
@@ -25,6 +27,13 @@ const addEmployee = async () => {
   //fname, lname, role_id, manager_id
   const employeeInfo = await viewEmployee();
   const roleInfo = await viewRole();
+
+  const employeePrompt= employeeInfo.map (() =>{
+    return {
+      name: employeeInfo.fname + " " + employeeInfo.lname,
+      value: employeeInfo.id
+    }
+  })
   
 const rolePrompt= roleInfo.map((role) => {
   return {
@@ -32,13 +41,31 @@ const rolePrompt= roleInfo.map((role) => {
       value: role.id 
   }
 })
-  console.log(rolePrompt);
+
   const response = await inquirer.prompt([
     {
-      type:
+      type: "text",
+      message: "Please enter a first name",
+    },
+    {
+      type: "text",
+      message: "Please enter a last name",
+    },
+    {
+      type: "list",
+      message: "Please choose a manager",
+      name: "manager",
+      choices: employeePrompt
+    },
+    {
+      type: "list",
+      message: "Please choose a role",
+      name: "role",
+      choices: rolePrompt
     }
-
   ])
+    console.log(response.role);
+    console.log(response.manager)
 }
 //view all departments, view all roles, wiew all employees,
 //add a department, add a role, add an employee,
@@ -62,6 +89,22 @@ const start = async () => {
         {
           name: "View all employees",
           value: "VIEW EMP"
+        },
+        {
+          name: "Add new department",
+          value: "ADD DEPT"
+        },
+        {
+          name: "Add new role",
+          value: "NEW ROLE"
+        },
+        {
+          name: "Add new employee",
+          value: "NEW EMP"
+        },
+        {
+          name: "Update an employee role",
+          value: "UPDATE EMP ROLE"
         }
 
       ]
@@ -70,20 +113,30 @@ const start = async () => {
   const { selection } = response
 
   switch (selection) {
-    case "VIEW EMP":
-      viewEmployee()
-      break
-    case "VIEW ROLE":
-      viewRole()
-      break
     case "VIEW DEPT":
-      viewDepartment()
+      console.log('You chose department view')
+      viewDepartment();
+      break;
+    case "VIEW ROLE":
+      console.log('You chose role view')
+      viewRole();
+      break;
+    case "VIEW EMP":
+      console.log('You chose employee view')
+      viewEmployee();
       break
-
-
-
+    case "ADD DEPT":
+      console.log('You chose ADD department')
+      break;
+    case "ADD ROLE":
+      console.log('You chose ADD role')
+      break;
+    case "ADD EMP":
+      console.log('You chose ADD employee')
+      addEmployee();
+      break;
   }
-
 }
+
 sequelize.sync({ force: false }).then(start)
 
